cd9baaa839397ab904d42b450d6e4af435513783
akwanmaroso <akwanmaroso@gmail.com>
2020-04-24 00:45:46 +0800

    feat(function): add selection_sort method

---
+++ src/selection_sort/index.js
@@ -0,0 +1,3 @@
+const { selection_sort } = require('./selection_sort')
+
+selection_sort([4, 5, 2, 4, 2, 7, 1, 2, 45, 32]);
---
+++ src/selection_sort/selection_sort.js
@@ -0,0 +1,19 @@
+module.exports.selection_sort = arr => {
+  if (!Array.isArray(arr)) {
+    return 'Data must be array';
+  }
+  let min, temp;
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    min = i;
+    for (let j = i + 1; j <= n - 1; j++) {
+      if (arr[j] < arr[min]) {
+        min = j
+      }
+    }
+    temp = arr[min];
+    arr[min] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
