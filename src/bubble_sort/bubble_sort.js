89fde699675abb5ff90505474f0eb3659175ce0f
akwanmaroso <akwanmaroso@gmail.com>
2020-04-23 12:10:33 +0800

    feat(function): add bubble sort method

---
+++ src/bubble_sort/bubble_sort.js
@@ -0,0 +1,19 @@
+module.exports.bubble_sort = (arr) => {
+  let temp;
+  if (!Array.isArray(arr)) {
+    console.log('Data must be array');
+    return;
+  }
+  console.log(arr);
+  let len = arr.length - 1
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if(arr[j] > arr[j+1]){
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp
+      }
+    }
+  }
+  return arr;
+}
