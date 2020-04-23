88ff46f87fb1a757ec35539a65f21d6f28415ee4
akwanmaroso <akwanmaroso@gmail.com>
2020-04-24 00:21:13 +0800

    feat(function): add insertion_sort method

---
+++ src/insertion_sort/insertion_sort.js
@@ -0,0 +1,17 @@
+module.exports.insertion_sort = (arr) => {
+  if (!Array.isArray(arr)) {
+    return 'Data must be array';
+  }
+  let j, key;
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    key = arr[i];
+    j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
