5b93aabb2746780f032a4b857a2cbf467156e66d
akwanmaroso <akwanmaroso@gmail.com>
2020-04-24 23:03:34 +0800

    feat(function): add method shell sort

---
+++ src/shell_sort/index.js
@@ -0,0 +1,3 @@
+const { shell_sort } = require('./shell_sort');
+
+shell_sort([4, 5, 2, 4, 2, 7, 1, 2, 45, 32]);
---
+++ src/shell_sort/shell_sort.js
@@ -0,0 +1,21 @@
+module.exports.shell_sort = arr => {
+  if (!Array.isArray(arr)) {
+    return 'Data must be array';
+  }
+  let n = arr.length;
+  let slot = Math.floor(n / 2);
+  while (slot > 0) {
+    for (let i = slot; i < n; i++) {
+      let temp = arr[i];
+      let j = i;
+      while (j >= slot && arr[j - slot] > temp) {
+        arr[j] = arr[j - slot];
+        j -= slot;
+      }
+      arr[j] = temp;
+    }
+    slot = Math.floor(slot / 2)
+  }
+  console.log(arr);
+  return arr;
+}
