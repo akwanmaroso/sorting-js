c0de867f2d9455ff9ce66e15ac3a8364372ad993
akwanmaroso <akwanmaroso@gmail.com>
2020-04-24 23:51:52 +0800

    test(selection_sort): add unit testing

---
+++ src/selection_sort/__test__/selection_sort.spec.js
@@ -0,0 +1,14 @@
+const { selection_sort } = require('../selection_sort');
+
+test('bubble sort test sorting', () => {
+  const actual = selection_sort([10, 9, 8, 7, 6]);
+  const expected = [6, 7, 8, 9, 10]
+  expect(actual).toStrictEqual(expected);
+});
+
+test('bubble sort test value must be array', () => {
+  const actual = selection_sort('helo');
+  const expected = 'Data must be array';
+  expect(actual).toBe(expected);
+});
+
