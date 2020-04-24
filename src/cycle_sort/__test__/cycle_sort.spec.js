396cef4d50c3250afd39b19131da80b1bf8311ed
akwanmaroso <akwanmaroso@gmail.com>
2020-04-24 23:47:21 +0800

    test(cycle_sort): add unit testing

---
+++ src/cycle_sort/__test__/cycle_sort.spec.js
@@ -0,0 +1,14 @@
+const { cycle_sort } = require('../cycle_sort');
+
+test('bubble sort test sorting', () => {
+  const actual = cycle_sort([10, 9, 8, 7, 6]);
+  const expected = [6, 7, 8, 9, 10]
+  expect(actual).toStrictEqual(expected);
+});
+
+test('bubble sort test value must be array', () => {
+  const actual = cycle_sort('helo');
+  const expected = 'Data must be array';
+  expect(actual).toBe(expected);
+});
+
