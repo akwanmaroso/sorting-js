3ae872fa52be1669e2564284b843a0da46943056
akwanmaroso <akwanmaroso@gmail.com>
2020-04-25 00:24:51 +0800

    feat(function): add entry point

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const bubble_sort = require('./bubble_sort');
+const cycle_sort = require('./cycle_sort');
+const insertion_sort = require('./insertion_sort');
+const selection_sort = require('./selection_sort');
+const shell_sort = require('./shell_sort');
+
+module.exports = {
+  bubble_sort,
+  cycle_sort,
+  insertion_sort,
+  selection_sort,
+  shell_sort
+}
