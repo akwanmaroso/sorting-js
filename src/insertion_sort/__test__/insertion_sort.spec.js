92a2b94e134a4f9bd255f29cf8849c53296524fe
akwanmaroso <akwanmaroso@gmail.com>
2020-04-24 23:49:45 +0800

    test(insertion_sort): add unit testing

---
+++ src/insertion_sort/__test__/insertion_sort.spec.js
@@ -0,0 +1,14 @@
+const { insertion_sort } = require('../insertion_sort');
+
+test('bubble sort test sorting', () => {
+  const actual = insertion_sort([10, 9, 8, 7, 6]);
+  const expected = [6, 7, 8, 9, 10]
+  expect(actual).toStrictEqual(expected);
+});
+
+test('bubble sort test value must be array', () => {
+  const actual = insertion_sort('helo');
+  const expected = 'Data must be array';
+  expect(actual).toBe(expected);
+});
+
