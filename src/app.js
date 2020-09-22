84cf6d9ac74d0ea43b82cf86751f933fdbbe1951
akwanmaroso <akwanmaroso@gmail.com>
2020-09-22 12:05:39 +0800

    fix: fix import

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import bubble_sort from "./bubble_sort";
-import cycle_sort from "./cycle_sort";
-import insertion_sort from "./insertion_sort";
-import selection_sort from "./selection_sort";
-import shell_sort from "./shell_sort";
+import bubble_sort from "./bubble_sort/bubble_sort";
+import cycle_sort from "./cycle_sort/cycle_sort";
+import insertion_sort from "./insertion_sort/insertion_sort";
+import selection_sort from "./selection_sort/selection_sort";
+import shell_sort from "./shell_sort/shell_sort";
 
 export {
   bubble_sort,
