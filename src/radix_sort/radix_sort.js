e8a8837ce6f139c597adccf6766e9d1e0fbf9e5c
akwanmaroso <akwanmaroso@gmail.com>
2020-09-23 15:18:10 +0800

    feat: add radix sort

---
+++ src/radix_sort/radix_sort.js
@@ -0,0 +1,19 @@
+const radix_sort = (arr) => {
+  if (!Array.isArray(arr)) {
+    throw Error("Data must be array")
+  }
+
+  const maxNum = Math.max(...arr) * 10;
+  let divisor = 10;
+  while (divisor < maxNum) {
+    let buckets = [...Array(10)].map(() => []);
+    for (let num of arr) {
+      buckets[Math.floor((num % divisor) / (divisor / 10))].push(num);
+    }
+    arr = [].concat.apply([], buckets);
+    divisor *= 10;
+  }
+  return arr;
+}
+
+export default radix_sort;
